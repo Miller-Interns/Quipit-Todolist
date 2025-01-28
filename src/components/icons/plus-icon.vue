d1ce314e16c5610987c1508335d5f0c2d931441a
gail <gailquipit@gmail.com>
2025-01-28 11:46:41 +0800

    ui: add icons

--- src/components/icons/plus-icon.vue
+++ src/components/icons/plus-icon.vue
@@ -1,22 +1,27 @@
 <template>
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
-  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z"/>
-</svg>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    fill="currentColor"
+    class="bi bi-plus-circle-fill"
+    viewBox="0 0 16 16"
+  >
+    <path
+      d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z"
+    />
+  </svg>
 </template>
 
-<script setup lang="ts">
-
-</script>
-
 <style scoped>
-    svg {
-        color: hsl(160, 100%, 37%);
-        width: 30px;
-        height: 30px;
-        margin: 0;
-    }
+  svg {
+    color: hsl(160, 100%, 37%);
+    width: 30px;
+    height: 30px;
+    margin: 0;
+  }
 
-    svg:hover {
-        color: rgb(78, 137, 116);
-    }
-</style>    
+  svg:hover {
+    color: rgb(78, 137, 116);
+  }
+</style>
